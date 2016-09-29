ecacd3a9510598ee99647aaf6d7432e7261d4f8c
Deep Bundela <bundeladeep@gmail.com>
2016-09-29 16:18:21 +0530

    [ADD]payment_ccavenue: Add new payment gateway for INR

---
+++ payment_ccavenue/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'CCAvenue Payment Acquirer',
+    'category': 'Hidden',
+    'summary': 'Payment Acquirer: CCAvenue Implementation',
+    'description': """CCAvenue Payment Acquirer""",
+    'author': 'Deep Bundela',
+    'depends': ['payment'],
+    'data': [
+        'views/ccavenue.xml',
+        'views/payment_acquirer.xml',
+        'data/payment_ccavenue_data.xml',
+        'views/payment_ccavenue_template.xml',
+    ],
+}
---
+++ payment_ccavenue/models/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+import payment_acquirer
+import payment_transaction
