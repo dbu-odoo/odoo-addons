34fa8baaf37d83382ebc67cb1fb7e73ada461316
Deep Bundela <bundeladeep@gmail.com>
2016-09-29 16:27:55 +0530

    [ADD] payment_payumoney: Add payment gateway for INR

---
+++ payment_payumoney/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'PayuMoney Payment Acquirer',
+    'category': 'Payment Acquirer',
+    'summary': 'Payment Acquirer: PayuMoney Implementation',
+    'description': """
+    PayuMoney Payment Acquirer for India.
+
+    PayUmoney payment gateway supports only INR currency.
+    """,
+    'author': 'Deep Bundela'
+    'depends': ['payment'],
+    'data': [
+        'views/payment_views.xml',
+        'views/payment_payumoney_templates.xml',
+        'data/payment_acquirer_data.xml',
+    ],
+    'license': 'GPLv3',
+}
